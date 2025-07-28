ac776e4ffd6c00ea59c0c9ea1495f5bab53b8a29
Sumbal Hashmi <sumbalhashmi14@gmail.com>
2025-07-28 17:59:23 +0500

    completed

--- dsa-10/index.cpp
+++ dsa-10/index.cpp
@@ -3,32 +3,45 @@ using namespace std;
 
 int main () 
 {
-    int arr[6] = {1, 5, 8, 11, 14, 22};
+    // int arr[6] = {1, 5, 8, 11, 14, 22};
+    int arr[7] = {4, 5, 6, 7, 0, 1, 2};
+
     int target = 22;
     int size = sizeof(arr) / sizeof(int);
+    
+
+
     
 
     // solved this by binary search (iterator code)
     int st = 0;
     int end = size-1;
-   
 
-    while(st <= end) {
-      int mid = (st + end)/2;
-        if(target < arr[mid]) {
-            end = mid-1;
-        } else if (target > arr[mid]) {
-            st = mid + 1; 
-        } else if (target == arr[mid]) {
-            cout<<"target found " <<arr[mid]<<" ";
-            return 0;
-        } else {
-            cout<<"target not found"<<endl;
-            return 0; 
-        }
+    int mid = st + (end - st)/2;
+        cout<<mid<<endl;
+
+   
+    // 3 conditions 
+    // equal 
+    // greater 
+    // less 
+
+    // while(st <= end) {
+    //   int mid = (st + end)/2;
+    //     if(target < arr[mid]) {
+    //         end = mid-1;
+    //     } else if (target > arr[mid]) {
+    //         st = mid + 1; 
+    //     } else if (target == arr[mid]) {
+    //         cout<<"target found " <<arr[mid]<<" ";
+    //         return 0;
+    //     } else {
+    //         cout<<"target not found"<<endl;
+    //         return 0; 
+    //     }
 
         
-    }
+    // }
 
     
 }
