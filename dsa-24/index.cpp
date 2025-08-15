819d63979d888cc014561f3c4a6feea29b7a20de
Sumbal Hashmi <sumbalhashmi14@gmail.com>
2025-08-15 18:48:16 +0500

    today work completed

---
+++ dsa-24/index.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main() 
+{
+    vector<vector<int>> mat = {{1,2,3,4}, {7,8,9,10}, {20, 23, 34, 60}};
+    int m = mat.size();
+    int n = mat[0].size();
+
+    cout<<m <<endl;
+    cout<<n <<endl;
+
+
+
+}
