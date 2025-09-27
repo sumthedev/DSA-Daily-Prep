db517f2b941254c0df095867b6eb1e4c9f34854c
Sumbal Hashmi <sumbalhashmi14@gmail.com>
2025-09-27 23:16:45 +0500

    today woek completed

---
+++ dsa-39/index.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Teacher {
+    private: 
+    double salaray;
+    public:
+
+    string name;
+    string subject;
+    string department;
+
+    void changeDep(string dep) {
+        department = dep;
+    };
+
+    void setSal(double sal) {
+        salaray = sal;
+    };
+
+    double getSal() {
+        return salaray;
+    }
+    
+    
+};
+
+
+
+int main () 
+{
+  
+    Teacher t1;
+    t1.name = "Anaya";
+   
+    t1.setSal(2500);
+    cout<<t1.name<<endl;
+    cout<<t1.getSal()<<endl;
+    return 0;
+}
