507d4321c2e50490c2175d25beaf9358cd0d2c78
Sumbal Hashmi <sumbalhashmi14@gmail.com>
2025-10-20 15:04:20 +0500

    today work completed

--- dsa-42/index.cpp
+++ dsa-42/index.cpp
@@ -55,37 +55,88 @@ public:
         }
     }
 
-    void pop_front () {
-        
-        if(head == NULL) {
+    void pop_front()
+    {
+
+        if (head == NULL)
+        {
             return;
         }
 
-        Node* temp = head;
+        Node *temp = head;
         head = head->next;
         temp->next = NULL;
         delete temp;
-
-
     }
 
-
-    void pop_back () 
+    void pop_back()
     {
-        if(head == NULL) {
+        if (head == NULL)
+        {
             return;
         }
 
-        Node* temp = head;
+        Node *temp = head;
 
-        while(temp->next != tail) {
+        while (temp->next != tail)
+        {
             temp = temp->next;
         }
 
         temp->next = NULL;
         delete tail;
         tail = temp->next;
+    }
 
+    void insert(int val, int pos)
+    {
+        if (pos < 0)
+        {
+            cout << "inavlid position\n";
+            return;
+        }
+
+        if (pos == 0)
+        {
+            push_front(val);
+        }
+
+        Node *newNode = new Node(val);
+        Node *temp = head;
+
+        for (int i = 0; i < pos - 1; i++)
+        {
+            if (temp == NULL)
+            {
+                cout << "invalid position\n";
+                return;
+            }
+
+            temp = temp->next;
+        }
+
+        newNode->next = temp->next;
+        temp->next = newNode;
+    }
+
+    int search(int key)
+    {
+
+        Node *temp = head;
+        int idx = 0;
+
+        while (temp != NULL)
+        {
+            if (temp->data == key)
+            {
+                return idx;
+            }
+
+            temp = temp->next;
+            idx++;
+        }
+
+        return -1;
     }
 
     void print()
@@ -106,14 +157,16 @@ int main()
 {
     List ll;
     ll.push_front(3);
-    ll.push_front(2);  
+    ll.push_front(2);
     ll.push_front(1);
-   
+
     ll.push_back(5);
 
+    // ll.insert(45, 2);
     // ll.pop_front();
-    ll.pop_back();
+    // ll.pop_back();
     ll.print();
+    cout<<ll.search(3)<<endl;
 
     return 0;
 }
