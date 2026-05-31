b140193574abdbdc9fe23dd1b4eecc9a5d19e02b
Sumbal Hashmi <sumbalhashmi14@gmail.com>
2026-05-31 23:40:50 +0500

    dsa daily prep 3 day

--- dsa-again-start/dsa-03/index.cpp
+++ dsa-again-start/dsa-03/index.cpp
@@ -4,5 +4,31 @@ using namespace std;
 
 int main () 
 {
-    
+    int n = 5; 
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    // Brute Force approch
+
+    // for (int st = 0; st<n; st++) {
+    //     for(int end = st; end<n; end++) {
+    //      for(int i=st; i<=end; i++) {
+    //         cout << arr[i];
+    //      }
+
+    //      cout << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+     int maxSum = 0;
+
+      for (int st = 0; st<n; st++) {
+        int currentSum = 0;
+        for(int end = st; end<n; end++) {    
+            currentSum += arr[end];
+            maxSum = max(currentSum, maxSum)  ; 
+        }
+        
+    }
+      cout << maxSum;
 }
