a776f072c7c26b7882a023a085297b644db71529
Sumbal Hashmi <sumbalhashmi14@gmail.com>
2026-09-07 03:11:29 +0500

    stacl concepts completed

---
+++ Stack/index-02.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
+using namespace std;
+
+
+// Stack implemntation using STL libaray
+
+int main () 
+{
+
+    stack<int> s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    while(!s.empty()) {
+        cout << s.top() << " ";
+        s.pop();
+    }
+
+    cout << endl;
+    return 0;
+}
