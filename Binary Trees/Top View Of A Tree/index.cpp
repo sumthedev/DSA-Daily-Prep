b3f65aaab5609b38d6eb775862cda1b05968031d
Sumbal Hashmi <sumbalhashmi14@gmail.com>
2026-09-22 00:39:20 +0500

    solved bottom view of the tree problem

--- Binary Trees/Top View Of A Tree/index.cpp
+++ Binary Trees/Top View Of A Tree/index.cpp
@@ -70,13 +70,47 @@ void topView(Node *root)
     cout << endl;
 }
 
+void bottomView(Node *root)
+{
+    queue<pair<Node *, int>> q;
+    map<int, int> m;
+    q.push({root, 0});
+
+    while (q.size() > 0)
+    {
+        Node *currNode = q.front().first;
+        int currHD = q.front().second;
+        q.pop();
+
+        // always update
+        m[currHD] = currNode->data;
+
+        if (currNode->left != NULL)
+        {
+            q.push({currNode->left, currHD - 1});
+        }
+        if (currNode->right != NULL)
+        {
+            q.push({currNode->right, currHD + 1});
+        }
+    }
+
+    for (auto it : m)
+    {
+        cout << it.second << " ";
+    }
+
+    cout << endl;
+}
+
 int main()
 {
     vector<int> preOrder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
 
     Node *root = buildTree(preOrder);
 
-    topView(root);
+    // topView(root);
+     bottomView(root);
 
     return 0;
 }
